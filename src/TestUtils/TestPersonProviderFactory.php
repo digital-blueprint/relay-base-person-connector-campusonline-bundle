dcbf338061d221d448a5a11e744c4201cdf13218
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2026-04-23 09:35:16 +0200

    add TestPersonProviderFactory

--- src/TestUtils/TestPersonProviderFactory.php
+++ src/TestUtils/TestPersonProviderFactory.php
@@ -4,7 +4,168 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BasePersonConnectorCampusonlineBundle\TestUtils;
 
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\DependencyInjection\Configuration;
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\DependencyInjection\DbpRelayBasePersonConnectorCampusonlineExtension;
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\Entity\CachedPerson;
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\Entity\CachedPersonStaging;
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\EventSubscriber\PersonEventSubscriber;
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\Service\PersonProvider;
+use Dbp\Relay\CoreBundle\TestUtils\TestEntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use Psr\Log\NullLogger;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 class TestPersonProviderFactory
 {
+    public const STAFF_USER_IDENTIFIER = 'staff-id';
+    public const STUDENT_USER_IDENTIFIER = 'student-id';
+    public const ALUMNS_USER_IDENTIFIER = 'alumnus-id';
 
+    private const EMAIL_ATTRIBUTE = 'email';
+    private const EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE = 'employeeWorkAddress';
+
+    public static function createTestPersonProvider(ContainerInterface $container, array $personEventSubscribers = []): PersonProvider
+    {
+        $entityManager = TestEntityManager::setUpEntityManager($container,
+            DbpRelayBasePersonConnectorCampusonlineExtension::ENTITY_MANAGER_ID);
+
+        $eventDispatcher = new EventDispatcher();
+        $personProvider = new PersonProvider(
+            $entityManager, new ArrayAdapter(), $eventDispatcher);
+        $personProvider->setLogger(new NullLogger());
+        $personProvider->setConfig(self::getConfig());
+
+        $personEventSubscriber = new PersonEventSubscriber($personProvider);
+        $personEventSubscriber->setConfig(self::getConfig());
+        $eventDispatcher->addSubscriber($personEventSubscriber);
+        foreach ($personEventSubscribers as $subscriber) {
+            $eventDispatcher->addSubscriber($subscriber);
+        }
+
+        self::recreatePersonCache($personProvider, $entityManager);
+
+        return $personProvider;
+    }
+
+    private static function getConfig(): array
+    {
+        return [
+            Configuration::CURRENT_PERSON_IDENTIFIER_EXPRESSION_ATTRIBUTE => Configuration::CURRENT_PERSON_IDENTIFIER_EXPRESSION_ATTRIBUTE_DEFAULT,
+            Configuration::CAMPUS_ONLINE_NODE => [
+                Configuration::BASE_URL_NODE => 'https://campusonline.at/campusonline/ws/public/rest/',
+                Configuration::CLIENT_ID_NODE => 'client',
+                Configuration::CLIENT_SECRET_NODE => 'secret',
+            ],
+            'local_data_mapping' => [
+                [
+                    'local_data_attribute' => self::EMAIL_ATTRIBUTE,
+                    'source_attribute' => CachedPerson::EMAIL_COLUMN_NAME,
+                    'default_value' => '',
+                ],
+                [
+                    'local_data_attribute' => self::EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE,
+                    'source_attribute' => PersonEventSubscriber::EMPLOYEE_POSTAL_ADDRESS_SOURCE_ATTRIBUTE,
+                    'default_value' => '',
+                ],
+            ],
+        ];
+    }
+
+    private static function recreatePersonCache(PersonProvider $personProvider, EntityManagerInterface $entityManager): void
+    {
+        self::mockResponsesForPersonCacheRecreation($personProvider);
+        try {
+            // this is expected to fail, since sqlite does not support some operations
+            $personProvider->recreatePersonsCache();
+        } catch (\Throwable) {
+            $personsLiveTable = CachedPerson::TABLE_NAME;
+            $personsStagingTable = CachedPersonStaging::TABLE_NAME;
+            $personsTempTable = 'organizations_old';
+            $connection = $entityManager->getConnection();
+            $connection->executeStatement("ALTER TABLE $personsLiveTable RENAME TO $personsTempTable;");
+            $connection->executeStatement("ALTER TABLE $personsStagingTable RENAME TO $personsLiveTable;");
+            $connection->executeStatement("ALTER TABLE $personsTempTable RENAME TO $personsStagingTable;");
+        } finally {
+            $personProvider->reset(); // ensure new api connection is created on subsequent requests
+        }
+    }
+
+    private static function mockResponsesForPersonCacheRecreation(PersonProvider $personProvider): void
+    {
+        $responses = [...self::createMockAuthServerResponses(),
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                file_get_contents(__DIR__.'/users_api_response.json')),
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                file_get_contents(__DIR__.'/person_claims_api_response.json')),
+        ];
+
+        $stack = HandlerStack::create(new MockHandler($responses));
+        $personProvider->setClientHandler($stack);
+    }
+
+    public static function mockPersonClaimsApiResponse(PersonProvider $personProvider): void
+    {
+        $responses = [...self::createMockAuthServerResponses(),
+            new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                file_get_contents(__DIR__.'/person_claims_api_response.json')),
+        ];
+
+        $stack = HandlerStack::create(new MockHandler($responses));
+        $personProvider->setClientHandler($stack);
+    }
+
+    public static function mockUserApiResponse(PersonProvider $personProvider): void
+    {
+        self::mockUserApiResponseWithContent($personProvider,
+            file_get_contents(__DIR__.'/users_api_response.json')
+        );
+    }
+
+    public static function mockEmptyUserApiResponse(PersonProvider $personProvider): void
+    {
+        self::mockUserApiResponseWithContent($personProvider,
+            file_get_contents(__DIR__.'/empty_api_response.json')
+        );
+    }
+
+    public static function mockUserApiResponse404(PersonProvider $personProvider): void
+    {
+        self::mockUserApiResponseWithContent($personProvider,
+            file_get_contents(__DIR__.'/empty_api_response.json'),
+            404
+        );
+    }
+
+    public static function mockUserApiResponseWithContent(PersonProvider $personProvider, string $content, int $status = 200): void
+    {
+        $responses = [...self::createMockAuthServerResponses(),
+            new Response(
+                $status,
+                ['Content-Type' => 'application/json'],
+                $content),
+        ];
+
+        $stack = HandlerStack::create(new MockHandler($responses));
+        $personProvider->setClientHandler($stack);
+    }
+
+    public static function createMockAuthServerResponses(): array
+    {
+        return [
+            new Response(200, ['Content-Type' => 'application/json;charset=utf-8'], '{"authServerUrl": "https://auth-server.net/"}'),
+            new Response(200, ['Content-Type' => 'application/json;charset=utf-8'], '{"token_endpoint": "https://token-endpoint.net/"}'),
+            new Response(200, ['Content-Type' => 'application/json;charset=utf-8'], '{"access_token": "token", "expires_in": 3600, "token_type": "Bearer"}'),
+        ];
+    }
 }
--- tests/PersonProviderTest.php
+++ tests/PersonProviderTest.php
@@ -6,102 +6,40 @@ namespace Dbp\Relay\BasePersonConnectorCampusonlineBundle\Tests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use Dbp\Relay\BasePersonBundle\Entity\Person;
-use Dbp\Relay\BasePersonConnectorCampusonlineBundle\DependencyInjection\Configuration;
-use Dbp\Relay\BasePersonConnectorCampusonlineBundle\DependencyInjection\DbpRelayBasePersonConnectorCampusonlineExtension;
-use Dbp\Relay\BasePersonConnectorCampusonlineBundle\Entity\CachedPerson;
-use Dbp\Relay\BasePersonConnectorCampusonlineBundle\Entity\CachedPersonStaging;
-use Dbp\Relay\BasePersonConnectorCampusonlineBundle\EventSubscriber\PersonEventSubscriber;
 use Dbp\Relay\BasePersonConnectorCampusonlineBundle\Service\PersonProvider;
+use Dbp\Relay\BasePersonConnectorCampusonlineBundle\TestUtils\TestPersonProviderFactory;
 use Dbp\Relay\CoreBundle\Exception\ApiError;
 use Dbp\Relay\CoreBundle\Rest\Options;
 use Dbp\Relay\CoreBundle\TestUtils\TestAuthorizationService;
-use Dbp\Relay\CoreBundle\TestUtils\TestEntityManager;
-use Doctrine\ORM\EntityManager;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Response;
-use Psr\Log\NullLogger;
-use Symfony\Component\Cache\Adapter\ArrayAdapter;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * TODO: add more persons to responses for reasonable pagination, search and filter tests.
  */
 class PersonProviderTest extends ApiTestCase
 {
-    private const TEST_USER_IDENTIFIER = '019ce14a-607b-7220-8a50-21a4dc9fdaf8';
+    private const STAFF_USER_IDENTIFIER = TestPersonProviderFactory::STAFF_USER_IDENTIFIER;
 
     private const EMAIL_ATTRIBUTE = 'email';
     private const EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE = 'employeeWorkAddress';
 
     private ?PersonProvider $personProvider = null;
-    private ?EntityManager $entityManager = null;
-
-    private static function getConfig(): array
-    {
-        return [
-            Configuration::CURRENT_PERSON_IDENTIFIER_EXPRESSION_ATTRIBUTE => Configuration::CURRENT_PERSON_IDENTIFIER_EXPRESSION_ATTRIBUTE_DEFAULT,
-            Configuration::CAMPUS_ONLINE_NODE => [
-                Configuration::BASE_URL_NODE => 'https://campusonline.at/campusonline/ws/public/rest/',
-                Configuration::CLIENT_ID_NODE => 'client',
-                Configuration::CLIENT_SECRET_NODE => 'secret',
-            ],
-            'local_data_mapping' => [
-                [
-                    'local_data_attribute' => self::EMAIL_ATTRIBUTE,
-                    'source_attribute' => CachedPerson::EMAIL_COLUMN_NAME,
-                    'default_value' => '',
-                ],
-                [
-                    'local_data_attribute' => self::EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE,
-                    'source_attribute' => PersonEventSubscriber::EMPLOYEE_POSTAL_ADDRESS_SOURCE_ATTRIBUTE,
-                    'default_value' => '',
-                ],
-            ],
-        ];
-    }
-
-    private static function createMockAuthServerResponses(): array
-    {
-        return [
-            new Response(200, ['Content-Type' => 'application/json;charset=utf-8'], '{"authServerUrl": "https://auth-server.net/"}'),
-            new Response(200, ['Content-Type' => 'application/json;charset=utf-8'], '{"token_endpoint": "https://token-endpoint.net/"}'),
-            new Response(200, ['Content-Type' => 'application/json;charset=utf-8'], '{"access_token": "token", "expires_in": 3600, "token_type": "Bearer"}'),
-        ];
-    }
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        $container = self::bootKernel()->getContainer();
-
-        $this->entityManager = TestEntityManager::setUpEntityManager($container,
-            DbpRelayBasePersonConnectorCampusonlineExtension::ENTITY_MANAGER_ID);
-
-        $eventDispatcher = new EventDispatcher();
-        $this->personProvider = new PersonProvider(
-            $this->entityManager, new ArrayAdapter(), $eventDispatcher);
-        $this->personProvider->setLogger(new NullLogger());
-        $this->personProvider->setConfig(self::getConfig());
-
-        $personEventSubscriber = null;
-        $personEventSubscriber = new PersonEventSubscriber($this->personProvider);
-        $personEventSubscriber->setConfig(self::getConfig());
-        $eventDispatcher->addSubscriber($personEventSubscriber);
-
-        $this->recreatePersonCache();
+        $this->personProvider = TestPersonProviderFactory::createTestPersonProvider(self::bootKernel()->getContainer());
         $this->login();
     }
 
     public function testGetCurrentPerson(): void
     {
         $currentPerson1 = $this->personProvider->getCurrentPerson();
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $currentPerson1->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $currentPerson1->getIdentifier());
         $this->assertNull($currentPerson1->getLocalData());
 
         $currentPerson2 = $this->personProvider->getCurrentPerson();
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $currentPerson2->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $currentPerson2->getIdentifier());
         $this->assertNull($currentPerson2->getLocalData());
         $this->assertSame($currentPerson1, $currentPerson2);
 
@@ -109,13 +47,13 @@ class PersonProviderTest extends ApiTestCase
         $options = [];
         Options::requestLocalDataAttributes($options, [self::EMAIL_ATTRIBUTE]);
         $currentPerson3 = $this->personProvider->getCurrentPerson($options);
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $currentPerson3->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $currentPerson3->getIdentifier());
         $this->assertCount(1, $currentPerson3->getLocalData());
         $this->assertSame('max.mustermann@someuni.at', $currentPerson3->getLocalData()[self::EMAIL_ATTRIBUTE]);
         $this->assertNotSame($currentPerson1, $currentPerson3);
 
         // same request, but with different local data requested
-        $this->mockPersonClaimsApiResponse(); // getting employee address should trigger a new api request
+        TestPersonProviderFactory::mockPersonClaimsApiResponse($this->personProvider); // getting employee address should trigger a new api request
         $options = [];
         Options::requestLocalDataAttributes($options, [self::EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE]);
         $currentPerson4 = $this->personProvider->getCurrentPerson($options);
@@ -131,7 +69,7 @@ class PersonProviderTest extends ApiTestCase
 
     public function testGetCurrentPersonWithLocalData(): void
     {
-        $this->mockPersonClaimsApiResponse(); // getting employee address should trigger a new api request
+        TestPersonProviderFactory::mockPersonClaimsApiResponse($this->personProvider); // getting employee address should trigger a new api request
 
         $options = [];
         Options::requestLocalDataAttributes($options, [self::EMAIL_ATTRIBUTE, self::EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE]);
@@ -160,8 +98,8 @@ class PersonProviderTest extends ApiTestCase
 
     public function testGetPerson(): void
     {
-        $person = $this->personProvider->getPerson(self::TEST_USER_IDENTIFIER);
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $person->getIdentifier());
+        $person = $this->personProvider->getPerson(self::STAFF_USER_IDENTIFIER);
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $person->getIdentifier());
         $this->assertNull($person->getLocalData());
     }
 
@@ -169,19 +107,19 @@ class PersonProviderTest extends ApiTestCase
     {
         $options = [];
         Options::requestLocalDataAttributes($options, [self::EMAIL_ATTRIBUTE]);
-        $person = $this->personProvider->getPerson(self::TEST_USER_IDENTIFIER, $options);
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $person->getIdentifier());
+        $person = $this->personProvider->getPerson(self::STAFF_USER_IDENTIFIER, $options);
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $person->getIdentifier());
         $this->assertCount(1, $person->getLocalData());
         $this->assertSame('max.mustermann@someuni.at', $person->getLocalData()[self::EMAIL_ATTRIBUTE]);
     }
 
     public function testGetPersonWithLocalDataNewRequest(): void
     {
-        $this->mockPersonClaimsApiResponse(); // getting employee address should trigger a new api request
+        TestPersonProviderFactory::mockPersonClaimsApiResponse($this->personProvider); // getting employee address should trigger a new api request
 
         $options = [];
         Options::requestLocalDataAttributes($options, [self::EMAIL_ATTRIBUTE, self::EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE]);
-        $person = $this->personProvider->getPerson(self::TEST_USER_IDENTIFIER, $options);
+        $person = $this->personProvider->getPerson(self::STAFF_USER_IDENTIFIER, $options);
         $this->assertCount(2, $person->getLocalData());
         $this->assertSame('max.mustermann@someuni.at', $person->getLocalData()[self::EMAIL_ATTRIBUTE]);
 
@@ -198,7 +136,7 @@ class PersonProviderTest extends ApiTestCase
         $persons = $this->personProvider->getPersons(1, 10);
         $this->assertCount(1, $persons);
         $person = $persons[0];
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $person->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $person->getIdentifier());
         $this->assertSame('Max', $person->getGivenName());
         $this->assertSame('Mustermann', $person->getFamilyName());
     }
@@ -216,7 +154,7 @@ class PersonProviderTest extends ApiTestCase
         $persons = $this->personProvider->getPersons(1, 10, $options);
         $this->assertCount(1, $persons);
         $person = $persons[0];
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $person->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $person->getIdentifier());
         $this->assertSame('Max', $person->getGivenName());
         $this->assertSame('Mustermann', $person->getFamilyName());
         $this->assertCount(1, $person->getLocalData());
@@ -225,14 +163,14 @@ class PersonProviderTest extends ApiTestCase
 
     public function testGetPersonsWithLocalDataNewRequest(): void
     {
-        $this->mockPersonClaimsApiResponse(); // getting employee address should trigger a new api request
+        TestPersonProviderFactory::mockPersonClaimsApiResponse($this->personProvider); // getting employee address should trigger a new api request
 
         $options = [];
         Options::requestLocalDataAttributes($options, [self::EMAIL_ATTRIBUTE, self::EMPLOYEE_POSTAL_ADDRESS_ATTRIBUTE]);
         $persons = $this->personProvider->getPersons(1, 10, $options);
         $this->assertCount(1, $persons);
         $person = $persons[0];
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $person->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $person->getIdentifier());
         $this->assertSame('Max', $person->getGivenName());
         $this->assertSame('Mustermann', $person->getFamilyName());
         $this->assertCount(2, $person->getLocalData());
@@ -254,7 +192,7 @@ class PersonProviderTest extends ApiTestCase
         $persons = $this->personProvider->getPersons(1, 10, $options);
         $this->assertCount(1, $persons);
         $person = $persons[0];
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $person->getIdentifier());
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $person->getIdentifier());
         $this->assertSame('Max', $person->getGivenName());
         $this->assertSame('Mustermann', $person->getFamilyName());
 
@@ -273,41 +211,41 @@ class PersonProviderTest extends ApiTestCase
 
     public function testGetPersonIdentifierByUsername(): void
     {
-        $this->mockUserApiResponse();
+        TestPersonProviderFactory::mockUserApiResponse($this->personProvider);
 
         $personIdentifier = $this->personProvider->getPersonIdentifierByUsername('maxm');
-        $this->assertEquals('019ce14a-607b-7220-8a50-21a4dc9fdaf8', $personIdentifier);
+        $this->assertEquals('staff-id', $personIdentifier);
 
-        $this->mockEmptyUserApiResponse();
+        TestPersonProviderFactory::mockEmptyUserApiResponse($this->personProvider);
 
         $this->assertNull($this->personProvider->getPersonIdentifierByUsername('foo'));
     }
 
     public function testGetPersonIdentifierByEmail(): void
     {
-        $this->mockUserApiResponse();
+        TestPersonProviderFactory::mockUserApiResponse($this->personProvider);
 
         $personIdentifier = $this->personProvider->getPersonIdentifierByEmail('max.mustermann@someuni.at');
-        $this->assertEquals('019ce14a-607b-7220-8a50-21a4dc9fdaf8', $personIdentifier);
+        $this->assertEquals('staff-id', $personIdentifier);
 
-        $this->mockEmptyUserApiResponse();
+        TestPersonProviderFactory::mockEmptyUserApiResponse($this->personProvider);
 
         $this->assertNull($this->personProvider->getPersonIdentifierByEmail('foo'));
     }
 
     public function testGetUserFromApiCached(): void
     {
-        $this->mockUserApiResponse();
+        TestPersonProviderFactory::mockUserApiResponse($this->personProvider);
 
-        $user = $this->personProvider->getUserFromApiCached(self::TEST_USER_IDENTIFIER);
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $user->getPersonUid());
+        $user = $this->personProvider->getUserFromApiCached(self::STAFF_USER_IDENTIFIER);
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $user->getPersonUid());
         $this->assertSame('max.mustermann@someuni.at', $user->getEmail(0));
         $this->assertSame('maxm', $user->getUsername(0));
     }
 
     public function testGetUserFromApiCachedNotFound(): void
     {
-        $this->mockUserApiResponse404();
+        TestPersonProviderFactory::mockUserApiResponse404($this->personProvider);
 
         try {
             $this->personProvider->getUserFromApiCached('foo');
@@ -319,14 +257,14 @@ class PersonProviderTest extends ApiTestCase
 
     public function testRequestCacheCurrentResultUsers(): void
     {
-        $this->personProvider->getPerson(self::TEST_USER_IDENTIFIER); // caches the person ids of current request result
+        $this->personProvider->getPerson(self::STAFF_USER_IDENTIFIER); // caches the person ids of current request result
 
-        $this->mockUserApiResponse();
+        TestPersonProviderFactory::mockUserApiResponse($this->personProvider);
         $this->personProvider->requestCacheCurrentResultUsers();
 
         // NOTE: no more requests must be made
-        $user = $this->personProvider->getUserFromApiCached(self::TEST_USER_IDENTIFIER);
-        $this->assertSame(self::TEST_USER_IDENTIFIER, $user->getPersonUid());
+        $user = $this->personProvider->getUserFromApiCached(self::STAFF_USER_IDENTIFIER);
+        $this->assertSame(self::STAFF_USER_IDENTIFIER, $user->getPersonUid());
         $this->assertSame('max.mustermann@someuni.at', $user->getEmail(0));
         $this->assertSame('maxm', $user->getUsername(0));
     }
@@ -364,91 +302,7 @@ class PersonProviderTest extends ApiTestCase
         $this->assertNull($this->personProvider->isCurrentUserAnAlumni());
     }
 
-    private function mockResponsesForPersonCacheRecreation(): void
-    {
-        $responses = [...self::createMockAuthServerResponses(),
-            new Response(
-                200,
-                ['Content-Type' => 'application/json'],
-                file_get_contents(__DIR__.'/users_api_response.json')),
-            new Response(
-                200,
-                ['Content-Type' => 'application/json'],
-                file_get_contents(__DIR__.'/person_claims_api_response.json')),
-        ];
-
-        $stack = HandlerStack::create(new MockHandler($responses));
-        $this->personProvider->setClientHandler($stack);
-    }
-
-    private function mockPersonClaimsApiResponse(): void
-    {
-        $responses = [...self::createMockAuthServerResponses(),
-            new Response(
-                200,
-                ['Content-Type' => 'application/json'],
-                file_get_contents(__DIR__.'/person_claims_api_response.json')),
-        ];
-
-        $stack = HandlerStack::create(new MockHandler($responses));
-        $this->personProvider->setClientHandler($stack);
-    }
-
-    private function mockUserApiResponse(): void
-    {
-        $this->mockUserApiResponseWithContent(
-            file_get_contents(__DIR__.'/users_api_response.json')
-        );
-    }
-
-    private function mockEmptyUserApiResponse(): void
-    {
-        $this->mockUserApiResponseWithContent(
-            file_get_contents(__DIR__.'/empty_api_response.json')
-        );
-    }
-
-    private function mockUserApiResponse404(): void
-    {
-        $this->mockUserApiResponseWithContent(
-            file_get_contents(__DIR__.'/empty_api_response.json'),
-            404
-        );
-    }
-
-    private function mockUserApiResponseWithContent(string $content, int $status = 200): void
-    {
-        $responses = [...self::createMockAuthServerResponses(),
-            new Response(
-                $status,
-                ['Content-Type' => 'application/json'],
-                $content),
-        ];
-
-        $stack = HandlerStack::create(new MockHandler($responses));
-        $this->personProvider->setClientHandler($stack);
-    }
-
-    private function recreatePersonCache(): void
-    {
-        $this->mockResponsesForPersonCacheRecreation();
-        try {
-            // this is expected to fail, since sqlite does not support some operations
-            $this->personProvider->recreatePersonsCache();
-        } catch (\Throwable) {
-            $personsLiveTable = CachedPerson::TABLE_NAME;
-            $personsStagingTable = CachedPersonStaging::TABLE_NAME;
-            $personsTempTable = 'organizations_old';
-            $connection = $this->entityManager->getConnection();
-            $connection->executeStatement("ALTER TABLE $personsLiveTable RENAME TO $personsTempTable;");
-            $connection->executeStatement("ALTER TABLE $personsStagingTable RENAME TO $personsLiveTable;");
-            $connection->executeStatement("ALTER TABLE $personsTempTable RENAME TO $personsStagingTable;");
-        } finally {
-            $this->personProvider->reset(); // ensure new api connection is created on subsequent requests
-        }
-    }
-
-    private function login(?string $userIdentifier = self::TEST_USER_IDENTIFIER, array $userAttributes = []): void
+    private function login(?string $userIdentifier = self::STAFF_USER_IDENTIFIER, array $userAttributes = []): void
     {
         TestAuthorizationService::setUp($this->personProvider, $userIdentifier, $userAttributes);
     }
